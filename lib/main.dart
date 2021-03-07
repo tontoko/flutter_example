24f3ef95160955c59e35721a933925d9cf945f8e
Hiraki Tomohiko <hirakitomohiko@gmail.com>
2021-03-07 16:17:34 +0900

    codelab

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'test', home: RandomWords());
+    return MaterialApp(
+      title: 'test',
+      home: RandomWords(),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+    );
   }
 }
 
@@ -17,11 +23,40 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (BuildContext context) {
+        final tiles = _saved.map((WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final divided = ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList();
+        return Scaffold(
+            appBar: AppBar(
+              title: Text('saved'),
+            ),
+            body: ListView(children: divided));
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('generator')),
+      appBar: AppBar(
+        title: Text('generator'),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
+      ),
       body: _buildSuggestions(),
     );
   }
@@ -41,6 +76,28 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
-    return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+    final alreadySaved = _saved.contains((pair));
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+    // return Stack(alignment: const Alignment(0.6, 0.6), children: <Widget>[
+    //   ListTile(title: Text(pair.asPascalCase, style: _biggerFont)),
+    //   Container(
+    //       decoration: BoxDecoration(
+    //         color: Colors.black26,
+    //       ),
+    //       child: Text('aaaaa'))
+    // ]);
   }
 }
